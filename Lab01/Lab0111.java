5a7fffe759c813001db85cd042ac358a762263db
Kit.k <104343721+KittitatK@users.noreply.github.com>
2025-11-21 21:57:27 +0700

    Fix formatting of array initialization and loop

--- Lab01/Lab0111.java
+++ Lab01/Lab0111.java
@@ -25,9 +25,9 @@ public class Lab0111 {
         System.out.println("How Many Number Do You Want To Input");
         int n = input.nextInt();
 
-        int[] arr = new int[n];                // ✔️ สร้าง Array ขนาด N
+        int[] arr = new int[n];            
 
-        for(int i = 0; i < n; i++){            // ✔️ รับค่าลง Array
+        for(int i = 0; i < n; i++){           
             System.out.println("Enter number " + (i+1));
             arr[i] = input.nextInt();
         }
