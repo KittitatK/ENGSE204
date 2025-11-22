5d341626f2e31551852a1c42444352dd89446e0c
Kit.k <104343721+KittitatK@users.noreply.github.com>
2025-11-22 16:49:30 +0700

    Update Lab 1.1-1.13.java

--- Lab01/Lab0110.java
+++ Lab01/Lab0110.java
@@ -1,33 +1,27 @@
 package Lab01;
 import java.util.Scanner;
 
-
 public class Lab0110 {
 
-    
-    public static Double calculateArea(Double x,  Double y , Double mul){
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Width Number ");
-        x = input.nextDouble();
-
-        System.out.println("Enter Height Number ");
-        y = input.nextDouble();
-
-        mul = x * y;
-
-        return mul;
+    public static Double calculateArea(Double x, Double y){
+        return x * y;
     }
-
 
     public static void main(String[] eren){
         Scanner input = new Scanner(System.in);
 
         System.out.println("Find The Area (W x H)");
 
-        double sum = calculateArea(0.0, 0.0, 0.0);
+        System.out.print("Enter Width Number: ");
+        double width = input.nextDouble();
 
-        System.out.println("The Area Is "+ sum);
+        System.out.print("Enter Height Number: ");
+        double height = input.nextDouble();
+
+        double area = calculateArea(width, height);
+
+        System.out.println("The Area Is " + area);
 
         input.close();
     }
