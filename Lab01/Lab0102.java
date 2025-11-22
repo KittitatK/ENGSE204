668b221233c6d6f8c9f5e9037ed0cb8341720b04
Kit.k <104343721+KittitatK@users.noreply.github.com>
2025-11-22 15:58:21 +0700

    Update Lab 1.1-1.13.java

--- Lab01/Lab0102.java
+++ Lab01/Lab0102.java
@@ -1,22 +1,22 @@
 package Lab01;
-import java.util.Scanner; //เรียกใช้ Util เพื่อให้รับค่าอ่านค่า
+import java.util.Scanner; 
 
 
 public class Lab0102 {
     public static void main(String[] sumi) {
-        Scanner input = new Scanner(System.in);//นำคำสั่งในการอ่านค่าเข้า
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Rectangle calculate (Width x Height)");//ตั้งชื่อโปรแกรม
+        System.out.println("Rectangle calculate (Width x Height)");
 
         System.out.println("Enter Width");
-        Double num1 = input.nextDouble();//ให้ผู้ใช้กรอกค่าความกว้าง
+        Double num1 = input.nextDouble();
 
         System.out.println("Enter Height");
-        Double num2 = input.nextDouble();//ให้ผู้ใช้ใส่ความสูง
+        Double num2 = input.nextDouble();
 
         System.out.print("Result :");
-        System.out.print(num1 * num2);//แสดงผลลัพธ์
+        System.out.print(num1 * num2);
 
-        input.close();//ปิดScanner
+        input.close();
     }
-}//end class
+}
