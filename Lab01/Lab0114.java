ae215b68cf4b78a8f2995b192f6cbfc707b2ae55
KittitatK <skhoogkhogsung@gmail.com>
2025-11-23 13:42:15 +0700

    Update Lab1.1 - Lab1.14

--- Lab01/Lab0114.java
+++ Lab01/Lab0114.java
@@ -1,5 +1,99 @@
 package Lab01;
+import java.util.Scanner;
 
 public class Lab0114 {
+    public static void main(String[] minsw){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Minesweeper");
+
+        System.out.print("Enter Row : ");
+        int R = input.nextInt();
+
+        System.out.print("Enter Column : ");
+        int C = input.nextInt();
+        
+        char[][] target = new char[R][C];
+
+         System.out.println("Create map Coordinate (.) , Bomb (*)");
+         for (int i = 0; i < R ; i++){
+            for ( int j = 0 ; j < C ; j++){
+                target[i][j] = input.next().charAt(0);
+            }
+         }
+
+        int tlm = 0;
+        for (int i = 0; i < R; i++){
+            for (int j = 0; j < C; j++){
+                if (target[i][j] == '*'){
+                    tlm ++;
+                }
+            }
+        }
+
+        System.out.println("Which target you want to select (R = 0,C = 0)");
+        int TR = input.nextInt() ;
+        int TC = input.nextInt() ;
+        int mc = 0;
+
+        if (target[TR][TC] == '*') {
+            System.out.println("Mine");
+             
+        }else{
+            if (TR - 1 >= 0 && TC - 1 >= 0) {
+                if (target[TR-1][TC-1] == '*'){ 
+                    mc++;
+                }
+            }
+
+            if (TR - 1 >= 0) {
+                if (target[TR-1][TC] == '*'){
+                    mc++;
+                }
+            }
+
+            if (TR - 1 >= 0 && TC + 1 < C) {
+                if (target[TR-1][TC+1] == '*'){
+                    mc++;
+                }
+            }
+
+            if (TC - 1 >= 0) {
+                if (target[TR][TC-1] == '*'){
+                    mc++;
+                }
+            }
+
+            if (TC + 1 < C) {
+                if (target[TR][TC+1] == '*'){
+                    mc++;
+                }
+            }
+
+            if (TR + 1 < R && TC - 1 >= 0) {
+                if (target[TR+1][TC-1] == '*'){
+                    mc++;
+                }
+            }
+
+            if (TR + 1 < R) {
+                if (target[TR+1][TC] == '*'){
+                    mc++;
+                }
+            }
+
+            if (TR + 1 < R && TC + 1 < C) {
+                if (target[TR+1][TC+1] == '*'){
+                    mc++;
+                }
+            }
+
+            System.out.printf("Have %d Mines Around\n",mc);
+        }
+
+        
+        System.out.printf("Total Mines in map = %d\n", tlm);
+    
+    }
     
 }
